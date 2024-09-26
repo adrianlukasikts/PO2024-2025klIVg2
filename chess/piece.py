a20652648948328b249cba422b057145401f8531
adrianlukasik <adrian.lukasik@technischools.com>
2024-09-26 12:13:52 +0200

    Added color

--- chess/piece.py
+++ chess/piece.py
@@ -1,10 +1,16 @@
 from abc import ABC, abstractmethod
 
+from chess.colors import Color
+
+
 class Piece:
+    def __init__(self, color: Color = Color.WHITE):
+        self.color = color
+
     @abstractmethod
     def get_moves_set(self, x: int, y: int) -> list[tuple[int, int]]:
         pass
 
     @abstractmethod
-    def get_moves_between(self, x1:int, y1:int, x2:int, y2:int)->list[tuple[int, int]]:
+    def get_moves_between(self, x1: int, y1: int, x2: int, y2: int) -> list[tuple[int, int]]:
         pass
