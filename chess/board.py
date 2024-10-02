6fb8315ebed53326273a076df741d218b2b0f3d8
adrianlukasik <adrian.lukasik@technischools.com>
2024-10-02 14:37:29 +0200

    added move parser

--- chess/board.py
+++ chess/board.py
@@ -26,6 +26,8 @@ class Board:
             ] for white_piece, black_piece in zip(white_pieces, black_pieces)
         ]
         self.color_current_player = Color.WHITE
+        self.move_counter = 1
+        self.game_is_finished = False
 
     def print_board(self):
         for row in range(7, -1, -1):
@@ -61,18 +63,21 @@ class Board:
             self.grid[x2][y2].piece = self.grid[x1][y1].piece
             self.grid[x1][y1].piece = None
             self.color_current_player = Color(self.color_current_player.value * -1)
+            self.move_counter += 1
         else:
             raise InvalidMoveException()
 
     def get_pos_from_notation(self, notation:str):
         letter = notation[0] # FIXME
-        y = int(notation[1])
+        y = int(notation[1]) - 1
         x = ord(letter) - ord("A")
         return x, y
 
 
+
 board = Board()
 board.print_board()
+
 
 
 try:
@@ -85,11 +90,18 @@ try:
     # board.make_move(1,3,1,4)
     # board.print_board()
     # board.make_move(1,4, 0, 5)
-    board.make_move(*board.get_pos_from_notation("E2"),*board.get_pos_from_notation("E4"))
-    board.print_board()
-    board.make_move(3,6,3,4)
-    board.print_board()
-    board.make_move(4,3,3,4)
-    board.print_board()
+    # board.make_move(*board.get_pos_from_notation("E2"),*board.get_pos_from_notation("E4"))
+    # board.print_board()
+    # board.make_move(3,6,3,4)
+    # board.print_board()
+    # board.make_move(4,3,3,4)
+    # board.print_board()
+
+    while not board.game_is_finished:
+        board.print_board()
+        position = input("Podaj nastepny ruch: ")
+        position_split = position.split(" ")
+        print(position_split)
 except InvalidMoveException:
-    print("nuh uh")
+
+    print(f"Niepoprawny ruch nr: {board.move_counter}" )
